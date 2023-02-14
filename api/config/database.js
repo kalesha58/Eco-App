5bf17c59bc56d8bcd6758d6b7780662c0951275a
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-02-14 15:57:11 +0530

    checking db:)

--- api/config/database.js
+++ api/config/database.js
@@ -4,7 +4,8 @@ const connectDatabase = () => {
 
   mongoose
     .connect(process.env.DB_URI, {
-      
+      useNewUrlParser: true,
+
     })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
