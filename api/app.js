1964383e1ae53eae15441bbf7ac6e55bbabcbf4a
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-02-14 15:24:50 +0530

    Products_routes_added:)

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middleware/error");
 app.use(express.json())
 // {================================ROUTES_IMPORT====================}
 const product = require("./routes/productRoute");
 app.use("/api/v1", product);
+// {==================MIDLEWARE FOR ERROR=============================}
+app.use(errorMiddleware);
 module.exports = app;
