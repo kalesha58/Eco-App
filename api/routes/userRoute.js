9623d67cf08e9933f995f781800bf536dd207ed8
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-02-15 23:01:19 +0530

    admin)

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,36 @@
 const express = require("express");
-const { registerUser, loginUser, logout, getUserDetails } = require("../controller/userController");
+const {
+  registerUser,
+  loginUser,
+  logout,
+  getUserDetails,
+  updatePassword,
+  updateProfile,
+  getAllUser,
+  getSingleUser,
+  updateUserRole,
+  deleteUser,
+} = require("../controller/userController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 // {=======AUTHENTICATION OF USER STARTS HERE ===================}
-router.route("/me").get( getUserDetails);
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+// admin authority on users
+router
+  .route("/admin/users")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getAllUser);
+router
+  .route("/admin/user/:id")
+  .get(isAuthenticatedUser, authorizeRoles("admin"), getSingleUser);
+router
+  .route("/admin/user/:id")
+  .put(isAuthenticatedUser, authorizeRoles("admin"), updateUserRole);
+router
+  .route("/admin/user/:id")
+  .delete(isAuthenticatedUser, authorizeRoles("admin"), deleteUser);
 module.exports = router;
