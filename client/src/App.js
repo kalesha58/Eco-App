052d9470e08ea30fcc1c55eb31be6c0c40d038e6
kalesha58 <105914652+kalesha58@users.noreply.github.com>
2023-02-20 21:28:58 +0530

    Added_setup:)

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,11 @@
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import About from "./Pages/About";
-import Products from "./Pages/Products";
-import SingleProduct from "./Pages/SingleProduct";
-import Contact from "./Pages/Contact";
-import Cart from "./Pages/Cart";
-import Error from "./Pages/Error";
-import { GlobalStyle } from "./GlobalStyle";
-import { ThemeProvider } from "styled-components";
-import Header from "./Components/Header";
-import Footer from "./Components/Footer";
-import LoginSignup from "./Pages/LoginSignup";
-import ProtectedRoute from "./Pages/Route/ProtectedRoute";
-import Profile from "./Pages/Profile";
-import store from "./store";
-import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { loadUser } from "./Redux/Actions/userAction";
-import UserOptions from "./Components/UserOptions";
+
+import './App.css';
 
 function App() {
-  const { user, isAuthenticated } = useSelector((state) => state.user);
-  const theme = {
-    colors: {
-      heading: "rgb(24 24 29)",
-      text: "rgba(29 ,29, 29, .8)",
-      white: "#fff",
-      black: " #212529",
-      helper: "#8490ff",
-
-      bg: "#F6F8FA",
-      footer_bg: "#0a1435",
-      btn: "rgb(98 84 243)",
-      border: "rgba(98, 84, 243, 0.5)",
-      hr: "#ffffff",
-      gradient:
-        "linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
-      shadow:
-        "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
-      shadowSupport: " rgba(0, 0, 0, 0.16) 0px 1px 4px",
-    },
-    media: {
-      mobile: "768px",
-      tab: "998px",
-    },
-  };
-useEffect(()=>{
-  store.dispatch(loadUser());
-},[])
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <Header />
-        {isAuthenticated && <UserOptions user={user} />}
- 
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:keyword" element={<Products />} />
-          <Route path="/product/:id" element={<SingleProduct />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<LoginSignup />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/account" element={<Profile />} />
-          {/* <Route
-            path="/account"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          /> */}
-
-          <Route path="*" element={<Error />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </ThemeProvider>
+    <div className="App">
+    hii
+    </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-// import {AppProvider} from "./Context/ProductContext";
-import { Provider } from "react-redux";
-import store from "./store";
-import { positions, transitions, Provider as AlertProvider } from "react-alert";
-import AlertTemplate from "react-alert-template-basic";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-const options = {
-  timeout: 5000,
-  postion: positions.BOTTOM_CENTER,
-  transitions: transitions.SCALE,
-};
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <AlertProvider template={AlertTemplate} {...options}>
-      <App />
-    </AlertProvider>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+
